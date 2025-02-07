438d23d87dff8b95cd2b1805c552f47948f2e9a0
BugHunter-X <askzenai@gmail.com>
2025-02-07 21:06:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.writeHead(200);
+  response.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080);
+console.log('Server is running on port 8080');
+//Uncommon bug: Server might not shut down gracefully using server.close() due to event loop issues.
